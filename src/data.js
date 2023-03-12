8645c88b19ea13f4bcea524f35a39847ece5b4df
grantbirki <grantbirki@github.com>
2023-03-12 16:43:18 +0000

    update footnote

--- src/data.js
+++ src/data.js
@@ -65,7 +65,6 @@ export const footerData = {
     { ariaLabel: 'GitHub', icon: 'tabler:brand-github', href: 'https://github.com/caragolfoundation' },
   ],
   footNote: `
-    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 float-left rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
-    Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://github.com/caragolfoundation/caragolfoundation.org"> The Caragol Foundation</a> with ❤️
   `,
 };
