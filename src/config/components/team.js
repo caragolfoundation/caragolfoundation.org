2b7f3f31930b88bb08dac4492257f6fed08b795a
grantbirki <grantbirki@github.com>
2023-03-18 16:51:42 +0000

    update team page

--- src/config/components/team.js
+++ src/config/components/team.js
@@ -8,64 +8,84 @@ const team = {
   title: 'Our Team',
 
   // The subtitle of the team section
-  description: 'Meet the team behind this project',
+  description: 'Meet the team behind the Caragol Foundation',
 
   // Members of the team
   members: [
     {
       // The name of the first member
-      name: 'Neil Sims',
+      name: 'Stephen Caragol',
       // The job title of the first member
-      title: 'Vue.js Developer',
+      title: 'Co-Founder',
       // The avatar of the first member
-      avatar: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/neil-sims.png',
+      avatar: 'https://avatars.githubusercontent.com/u/7899881?s=60&v=4',
       // Social links of the first member
       social: {
-        website: '#',
+        // website: '#',
         // twitter: '#',
-        instagram: '#',
+        // instagram: '#',
         // facebook: '#',
-        linkedin: '#',
+        linkedin: 'https://www.linkedin.com/in/stephen-caragol-4994a233/',
         // github: '#',
       },
     },
     {
-      name: 'Bonnie Green',
-      title: 'CEO',
-      avatar: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png',
+      // The name of the first member
+      name: 'Michelle Caragol',
+      // The job title of the first member
+      title: 'Co-Founder',
+      // The avatar of the first member
+      avatar: 'https://avatars.githubusercontent.com/u/3055425?v=4',
+      // Social links of the first member
       social: {
         // website: '#',
-        twitter: '#',
+        // twitter: '#',
         // instagram: '#',
         // facebook: '#',
-        linkedin: '#',
-        github: '#',
+        linkedin: 'https://www.linkedin.com/in/michelle-caragol-a0223a46/',
+        // github: '#',
       },
     },
     {
-      name: 'Jese Leos',
-      title: 'UI/UX Designer',
-      avatar: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png',
+      name: 'Ian Caragol',
+      title: 'Board Member',
+      avatar:
+        'https://media.licdn.com/dms/image/C4E03AQHmdyJQkB5gTA/profile-displayphoto-shrink_400_400/0/1595873634804?e=1684368000&v=beta&t=VeNfugVfevrxFpoZBIyq2UwHIHOTAnPFbOVTLcEUtA4',
       social: {
         // website: '#',
         // twitter: '#',
-        instagram: '#',
+        // instagram: '#',
+        // facebook: '#',
+        linkedin: 'https://www.linkedin.com/in/iancaragol/',
+        github: 'https://github.com/iancaragol',
+      },
+    },
+    {
+      name: 'Leah Caragol',
+      title: 'Secretary',
+      avatar:
+        'https://media.licdn.com/dms/image/C4E03AQEg2sdMi3bFPA/profile-displayphoto-shrink_400_400/0/1649268916153?e=1684368000&v=beta&t=KdNyF5rF4ic9gep3puUILla82ZIcddV5mtxf-muQJVI',
+      social: {
+        // website: '#',
+        // twitter: '#',
+        instagram: 'https://www.instagram.com/leahcaragol/',
         facebook: '#',
         // linkedin: '#',
-        github: '#',
+        github: 'https://github.com/leahcaragol',
       },
     },
     {
-      name: 'Michael Gough',
-      title: 'Frontend Developer',
-      avatar: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gough.png',
+      name: 'Grant Birkinbine',
+      title: 'Web Developer',
+      avatar:
+        'https://media.licdn.com/dms/image/C5603AQH_1ow6RpwKRA/profile-displayphoto-shrink_400_400/0/1661623022105?e=1684368000&v=beta&t=9gOCM0Fbd2xUsnXc8VNZL6AP4deZqkswxpJsyyanVPg',
       social: {
-        // website: '#',
-        // twitter: '#',
-        instagram: '#',
+        website: 'https://birki.io',
+        twitter: 'https://twitter.com/grantbirki',
+        instagram: 'https://www.instagram.com/grantbirki/',
         // facebook: '#',
-        linkedin: '#',
-        github: '#',
+        linkedin: 'https://www.linkedin.com/in/grantbirkinbine/',
+        github: 'https://github.com/grantbirki',
       },
     },
   ],
