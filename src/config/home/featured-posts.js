e9d5f163797d041a7d161267b1feb2e6420606bb
grantbirki <grantbirki@github.com>
2023-03-18 11:03:33 +0000

    featured post section sync

--- src/config/home/featured-posts.js
+++ src/config/home/featured-posts.js
@@ -8,10 +8,7 @@ const featuredPosts = {
   title: 'Find out more content in our Blog',
 
   // The description of the component / information
-  information: `The blog is used to display AstroWind documentation.
-  Each new article will be an important step that you will need to know to be an expert in creating a website using Astro + Tailwind CSS.
-  Astro is a very interesting technology. Thanks.
-  `,
+  information: `The Caragol Foundation Blog`,
 
   // The list of posts to be displayed
   // These ids are the file names of the posts in the content/post folder
