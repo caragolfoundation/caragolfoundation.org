80fd9ac60f03d75e1c4aae7e586655ba4ff11210
grantbirki <grantbirki@github.com>
2023-03-18 11:02:16 +0000

    sync footer

--- src/config/components/footer.js
+++ src/config/components/footer.js
@@ -1,7 +1,7 @@
 // Configuration for the footer component on all pages
 
 // ----------- imports -----------
-import { getPermalink, getAsset } from '~/utils/permalinks';
+import { getPermalink, getBlogPermalink } from '~/utils/permalinks';
 // ----------- imports -----------
 
 const footerData = {
@@ -9,71 +9,45 @@ const footerData = {
   links: [
     {
       // The title of the section
-      title: 'Product',
+      title: 'Resources',
       // List of links to be displayed in the section
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
         { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
+        { text: 'Blog', href: getBlogPermalink() },
+        { text: 'Open Source', href: 'https://github.com/caragolfoundation' },
       ],
     },
     {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
+      title: 'Projects',
+      links: [{ text: 'Coming soon...', href: '#' }],
     },
     {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
+      title: 'Contact',
+      links: [{ text: 'Contact Page', href: getPermalink('/contact') }],
     },
   ],
 
   // Secondary links to be displayed in the footer
-  secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
-  ],
+  // secondaryLinks: [
+  //   { text: 'Terms', href: getPermalink('/terms') },
+  //   { text: 'Privacy Policy', href: getPermalink('/privacy') },
+  // ],
 
   // A list of social links to be displayed in the footer
   socialLinks: [
     { ariaLabel: 'Twitter', icon: 'tabler:brand-twitter', href: '#' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/grantbirki/astrowind' },
+    {
+      ariaLabel: 'Github',
+      icon: 'tabler:brand-github',
+      href: 'https://github.com/caragolfoundation/caragolfoundation.org',
+    },
   ],
 
   // The footnote to be displayed in the footer (can contain HTML)
   footNote: `
-    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 float-left rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
-    Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    Made by <a class="text-blue-600 hover:underline dark:text-gray-200" target="_blank" href="https://github.com/caragolfoundation"> The Caragol Foundation</a> with ❤️
   `,
 };
 
